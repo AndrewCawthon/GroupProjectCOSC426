e0820f59a89a2487bea12c963587ec0b0ab28e7b
NoahKingsley <nkingsle@emich.edu>
2022-03-24 17:17:06 -0400

    Added drive arrows to design, added the drawables as well

--- A12-BTver03/app/src/main/java/com/example/a12_btver03/RobotDriveScreen.java
+++ A12-BTver03/app/src/main/java/com/example/a12_btver03/RobotDriveScreen.java
@@ -1,6 +1,9 @@
 package com.example.a12_btver03;
 
+import android.graphics.ColorMatrixColorFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -59,11 +62,53 @@ public class RobotDriveScreen extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
                 if (lowerLimit <= 100) {
                     binding.sbPowerlow.setProgress(lowerLimit);
-                }
-                else if (lowerLimit >= 0) {
+                } else if (lowerLimit >= 0) {
                     binding.sbPowerlow.setProgress(lowerLimit);
                 }
             }
         });
+        binding.driveUp.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view){
+                /*possible feedback state
+                final float[] NEGATIVE = {
+                        -1.0f,     0,     0,    0, 255, // red
+                        0, -1.0f,     0,    0, 255, // green
+                        0,     0, -1.0f,    0, 255, // blue
+                        0,     0,     0, 1.0f,   0  // alpha
+                };
+
+                binding.driveUp.setColorFilter(new ColorMatrixColorFilter(NEGATIVE));*/
+                //insert drive forward
+                return false;
+            }
+        });
+        binding.driveRight.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view){
+                //feedback state
+
+                //insert drive right
+                return false;
+            }
+        });
+        binding.driveLeft.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view){
+                //feedback state
+
+                //insert drive left
+                return false;
+            }
+        });
+        binding.driveBack.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view){
+                //feedback state
+                //insert reverse
+                return false;
+            }
+        });
+
     }
 }
